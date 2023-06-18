630df2c5c727be7e4bb9c8fd583deba1d3044dd4
george-hub331 <idiaghegeorge9@gmail.com>
2023-06-18 02:57:14 +0100

    update: add serverless functions

---
+++ pages/api/auth/addDao.ts
@@ -0,0 +1,138 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import * as ethers from 'ethers';
+import Cryptr from "cryptr";
+import axios from "axios";
+import crypto from 'crypto';
+
+
+type Data = {
+  message: string;
+  token?: string;
+  data?: any;
+  error: boolean
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+
+  
+    if (req.method == 'POST') {
+
+      const { hash, metadata, contract, joined, name, desc, defCon } = req.body;
+
+      const validateAddress = ethers.verifyMessage(
+        "UseClover Signature Request \n\nSign To Continue \n",
+        hash
+      );
+
+      (async () => {
+
+        try {
+
+          if (validateAddress != metadata) {
+            throw {
+              status: 400,
+              response: {
+                data: {
+                  message: "Invalid address",
+                },
+              },
+            };
+          }
+
+          if (!name || desc.length > 300) {
+            throw {
+              status: 400,
+              response: {
+                data: {
+                  message: "Invalid name or description",
+                },
+              },
+            };
+          }
+
+          const keypair = await crypto.subtle.generateKey(
+            {
+              name: "ECDH",
+              namedCurve: "P-256",
+            },
+            true,
+            ["deriveKey", "deriveBits"]
+          );
+
+          const publicKey = await crypto.subtle.exportKey(
+            "jwk",
+            keypair.publicKey
+          );
+
+          const privateKey = await crypto.subtle.exportKey(
+            "jwk",
+            keypair.privateKey
+          );
+
+          const cryptr = new Cryptr(contract);
+
+          const crytr_def = new Cryptr(hash);
+
+
+          const group_keys_init = cryptr.encrypt(
+            JSON.stringify({ public: publicKey, private: privateKey })
+          );
+
+          const group_keys = crytr_def.encrypt(
+            JSON.stringify({ public: publicKey, private: privateKey })
+          );
+
+          const payload: any = {
+            contract,
+            joined,
+            desc: desc || "",
+            name,
+            metadata,
+            group_keys_init,
+            group_keys,
+            defCon,
+          };
+
+          const { data: {
+            token, 
+            data
+          }} = await axios.post('/daos/store', payload, {
+            baseURL: process.env.NEXT_PUBLIC_APP_URL || "",
+            headers: {
+              "X-App-Key": process.env.APP_KEY || "",
+            },
+          });
+
+          res.status(200).json({
+            message: "Successful",
+            token,
+            data,
+            error: false
+          })
+
+        }catch (err) {
+
+            const error = err as any;
+
+            console.log(error)
+
+            res.status(error?.status || 400).json({
+              error: true,
+              message: error?.response?.data?.message || "Something went wrong",
+            })
+
+        }
+
+      })()
+
+    }else{
+         res.status(422).json({
+           message: "Method not supported",
+           error: true,
+         });
+    }
+
+}
---
+++ pages/api/auth/test.ts
@@ -0,0 +1,125 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from "axios";
+import Cryptr from "cryptr";
+import { ethers } from "ethers";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  message: string;
+  error: boolean;
+  token?: string
+  dao?: any 
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  
+  if (req.method == 'POST') {
+
+    const { name, email, address, hash } = req.body;
+
+    if (name && email && address) {
+
+      (async () => {
+
+        const validateAddress = ethers.verifyMessage(
+          "UseClover Signature Request \n\nSign To Continue \n",
+          hash
+        );
+
+        try{
+
+          if (validateAddress != address) {
+            throw {
+              status: 400,
+              response: {
+                data: {
+                  message: "Invalid address",
+                },
+              },
+            };
+          }
+
+        const {
+          data: { group: { data: gdata } },
+        } = await axios.get("/test/1/1", {
+          baseURL: process.env.NEXT_PUBLIC_APP_URL || "",
+          headers: {
+            "X-App-Key": process.env.APP_KEY || "",
+          },
+        });
+
+        const groupData = JSON.parse(gdata);
+
+        let encryptedData = undefined;
+
+        if (groupData.admins !== undefined) {
+
+          const [ { key: encryptedKeys } ] = groupData.admins
+
+          const key = process.env.SIGNATURE || ""
+
+          const enc = new Cryptr(key);
+
+          const decryptedKeys = enc.decrypt(encryptedKeys);         
+
+          const enc_new = new Cryptr(hash);
+
+          encryptedData = enc_new.encrypt(decryptedKeys);
+
+        }
+
+        const { data } = await axios.post(
+          "/testing/store",
+          {
+            name,
+            email,
+            address,
+            group: encryptedData
+          },
+          {
+            baseURL: process.env.NEXT_PUBLIC_APP_URL || "",
+            headers: {
+              "X-App-Key": process.env.APP_KEY || "",
+            },
+          }
+        );
+
+        res.status(201).json({
+          message: "Success",
+          dao: {...data.dao, hash},
+          error: false,
+          token: data.token
+        })
+
+      }catch(err){
+
+          const error = err as any;
+
+          console.log(error)
+
+          res.status(error?.status || 400).json({
+            error: true,
+            message: error?.response?.data?.message || "Something went wrong, please try again"
+          })
+      }
+
+      })()
+
+    }else{
+      res.status(400).json({
+        message: "Missing required fields",
+        error: true,
+      })
+    }
+
+  }else{
+      res.status(422).json({
+          message: "Method not supported",
+          error: true,
+      });
+  }
+
+}
